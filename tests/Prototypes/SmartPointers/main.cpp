0f0f55b8cb12a4593270ceeed4060d1e66458b1f
CristianAtDev <webmaster@todoteca.zapto.org>
2014-03-05 16:16:35 +0100

    Arreglado test Unique_ptr, ahora funciona.

--- tests/Prototypes/SmartPointers/main.cpp
+++ tests/Prototypes/SmartPointers/main.cpp
@@ -14,11 +14,12 @@ class message{
             this->str = str;
         }
         bool operator==(message& m) const{
+            std::cout << "me han comparado" << std::endl;
             return (m.getString() == str);
         }
         inline const std::string getString(){ return str; }
         ~message(){
-            //std::cout << "DTOR "<< str << std::endl;
+            std::cout << "DTOR "<< str << std::endl;
         }
 };
 
@@ -29,6 +30,7 @@ std::shared_ptr<message> processMessageSPTR(std::shared_ptr<message> m){
 std::unique_ptr<message> processMessageUPTR(std::unique_ptr<message> m){
     std::cout << "Function In " << m->getString() << std::endl;
     std::cout << "Function Out " << m->getString() << std::endl;
+    return std::move(m);
 }
 void shared_ptrExample(){
     std::cout << "Shared Pointer:" << std::endl;
@@ -80,7 +82,6 @@ void shared_ptrExample(){
     std::cout << "Salimos Programa." << std::endl;
 }
 void unique_ptrExample(){
-    /*
     std::cout << "Unique Pointer " << std::endl;
     std::list<std::unique_ptr<message>> l1;
     std::list<std::unique_ptr<message>> l2;
@@ -101,12 +102,13 @@ void unique_ptrExample(){
         std::cout << "Entramos otro Scope" << std::endl;
             std::unique_ptr<message> spt2(new message("Mensaje 2"));
             std::unique_ptr<message> spt3(new message("Mensaje 3"));
+            spt2 == spt3;
             std::cout << "Añadimos Mensaje 2 al frente de la lista." << std::endl;
-            l1.push_front(spt2);
+            l1.push_front(std::move(spt2));
         std::cout << "Salimos Otro Scope" << std::endl;
     }
     std::cout << "Process Function Call" << std::endl;
-    processMessageUPTR(std::move(l1.front()));
+    l1.push_front(processMessageUPTR(std::move(l1.front())));
     std::cout << "Process Function End" << std::endl;
     {
         std::cout << "Copy Message 2" << std::endl;
@@ -116,14 +118,14 @@ void unique_ptrExample(){
         std::cout << "Message 2 Borrado" << std::endl;
         std::cout << "Contenido MSG 2 " << spt2->getString() << std::endl;
         std::cout << "añadimos MSG2 a l2 " << std::endl;
-        l2.push_back(spt2);
+        l2.push_back(std::move(spt2));
     }
     std::cout << "Obtenemos Mensaje Lista 2 " << l2.front()->getString() << std::endl;
     std::cout << "Salimos Programa." << std::endl;
-    */
 }
 int main(){
     //shared_ptrExample();
+    unique_ptrExample();
     return 0;
 }
 
