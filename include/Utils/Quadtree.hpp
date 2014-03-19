16984218d461f36861a58d8f9d965715bf1b2cf5
CristianAtDev <webmaster@todoteca.zapto.org>
2014-03-19 11:30:03 +0100

    Agregada especificacion de la clase Quadtree.

--- include/Utils/Quadtree.hpp
+++ include/Utils/Quadtree.hpp
@@ -1,13 +1,26 @@
 #ifndef QUADTREE_H
 #define QUADTREE_H
+#include <array>
+#include <SFML/Graphics/Rect.hpp>
 namespace ant{
     namespace Utils{
         class Quadtree{
             private:
-
+                const static int MAX_CAPACITY = 10;
+                const static int MAX_LEVEL = 5;
+                std::array<std::unique_ptr<Quadtree>,4> nodes;
+                sf::FloatRect bounds;
+                std::list<Entity*> entities;
+                void split();
             public:
-
+                Quadtree();
+                Quadtree(sf::FloatRect bounds);
+                int getIndex(Entity* e);
+                void insert(Entity* e);
+                void clear();
+                std::list<Entity*> retrieve(std::list<Entity*> entities,Entity* e);
+                ~Quadtree();
         };
-    }
-}
+    };
+};
 #endif
