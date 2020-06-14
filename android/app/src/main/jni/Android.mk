040117ac7fd55529b705aaa4e1908fcc2878db63
Cristian Gonzalez <cristian.glez.m@gmail.com>
2020-06-14 22:59:38 +0200

    Update Android.mk

--- android/app/src/main/jni/Android.mk
+++ android/app/src/main/jni/Android.mk
@@ -15,7 +15,7 @@ LOCAL_SRC_FILES += $(FILE_LIST:$(LOCAL_PATH)/%=%)
 FILE_LIST := $(wildcard $(LOCAL_PATH)/../../../../../src/*/*/*.cpp)
 LOCAL_SRC_FILES += $(FILE_LIST:$(LOCAL_PATH)/%=%)
 
-LOCAL_STATIC_LIBRARIES := JsonBox
+LOCAL_STATIC_LIBRARIES := jsonbox
 LOCAL_SHARED_LIBRARIES := sfml-system
 LOCAL_SHARED_LIBRARIES += sfml-window
 LOCAL_SHARED_LIBRARIES += sfml-graphics
